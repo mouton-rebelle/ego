d2c46d8b50192fba950e7d3374f1cf6bd8785bdb
Cédric FONTAINE <cf@eg0.me>
2016-03-09 20:08:46 +0100

    comment reducer

---
+++ src/redux/modules/comments.js
@@ -0,0 +1,103 @@
+import request from 'superagent-bluebird-promise'
+
+// saving a comment
+export const COM_SAVE_PENDING = 'COM_SAVE_PENDING'
+export const COM_SAVE_FULFILLED = 'COM_SAVE_FULFILLED'
+export const COM_SAVE_REJECTED = 'COM_SAVE_REJECTED'
+
+// showing / hiding comments for a post
+export const COM_SHOW_BYPOST = 'COM_SHOW_BYPOST'
+export const COM_HIDE_BYPOST = 'COM_HIDE_BYPOST'
+
+// loading comments for a post
+export const COM_LOAD_BYPOST_PENDING = 'COM_LOAD_BYPOST_PENDING'
+export const COM_LOAD_BYPOST_FULFILLED = 'COM_LOAD_BYPOST_FULFILLED'
+export const COM_LOAD_BYPOST_REJECTED = 'COM_LOAD_BYPOST_REJECTED'
+
+// loading recents comments
+export const COM_LOAD_RECENTS_PENDING = 'COM_LOAD_RECENTS_PENDING'
+export const COM_LOAD_RECENTS_FULFILLED = 'COM_LOAD_RECENTS_FULFILLED'
+export const COM_LOAD_RECENTS_REJECTED = 'COM_LOAD_RECENTS_REJECTED'
+
+const initialState = {
+  recents: [],
+  byPost: {},
+  shownForPost: [],
+  saving: false
+}
+
+export const showCommentsForPost = function (postId) {
+  return {
+    type: COM_SHOW_BYPOST,
+    payload: {
+      postId: postId
+    }
+  }
+}
+
+export const hideCommentsForPost = function (postId) {
+  return {
+    type: COM_HIDE_BYPOST,
+    payload: {
+      postId: postId
+    }
+  }
+}
+
+export const getCommentsForPost = function (postId) {
+  return {
+    type: 'COM_LOAD_BYPOST',
+    meta: {
+      postId: postId
+    },
+    payload: {
+      promise: request(`/api/comments/post/${postId}`).promise()
+    }
+  }
+}
+
+export const saveComment = function (comment) {
+  return {
+    types: 'COM_SAVE',
+    payload: {
+      promise: request.post('/api/comments').send(comment).promise()
+    }
+  }
+}
+
+export default function comments (state = initialState, action) {
+  switch (action.type) {
+    case COM_SHOW_BYPOST:
+      state.shownForPost = [...state.shownForPost, action.payload.postId]
+      return state
+
+    case COM_HIDE_BYPOST:
+      state.shownForPost = state.shownForPost.filter((p) => p !== action.payload.postId)
+      return state
+
+    case COM_SAVE_PENDING:
+      return {...state, saving: true}
+
+    case COM_SAVE_FULFILLED:
+      let com = action.payload.body
+      let postId = com.post
+      return {
+        saving: false,
+        shownForPost: state.shownForPost,
+        byPost: {
+          ...state.byPost,
+          [postId]: [...state.byPost[postId], com]
+        },
+        recents: [...state.recents, com]
+      }
+
+    case COM_LOAD_BYPOST_FULFILLED:      // parses our header "posts 30-40/2000"
+      return {...state,
+          byPost: {...state.byPost,
+            [action.meta.postId]: action.payload.body}
+      }
+
+    default:
+      return state
+  }
+}
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
-import counter from './modules/counter'
+import comments from './modules/comments'
 import posts from './modules/posts'
 
 export default combineReducers({
-  counter,
   posts,
+  comments,
   router
 })
