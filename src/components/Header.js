edb8d452791e17f80409dcd36297e8790fecb69c
Cédric Fontaine <cf@eg0.me>
2016-05-17 21:03:57 +0200

    played with header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router'
 
-export default () => {
-  return (
-    <header className='header'>
-      <h1 className='logo header__logo'>EG<span className='logo__alt'>0</span></h1>
-      <ul className='nav head-nav'>
-        <li className='head-nav__item'><Link className='head-nav__item__link' to='/'>Home</Link></li>
-        <li className='head-nav__item'><Link className='head-nav__item__link' to='/search'>Search</Link></li>
-        <li className='head-nav__item'><Link className='head-nav__item__link' to='/about'>About</Link></li>
-      </ul>
-    </header>
-  )
+export default class Header extends Component {
+
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      small: false
+    }
+  }
+
+  componentDidMount () {
+    window.addEventListener('scroll', () => {
+      this.setState({
+        small: window.scrollY>50
+      })
+    })
+  }
+
+  render () {
+    const classes = `header ${this.state.small ? 'header--small' : ''}`
+    return (
+      <header className={classes}>
+        <h1 className='logo header__logo'>EG<span className='logo__alt'>0</span></h1>
+        <ul className='nav head-nav'>
+          <li className='head-nav__item'><Link className='head-nav__item__link' to='/'>Home</Link></li>
+          <li className='head-nav__item'><Link className='head-nav__item__link' to='/search'>Search</Link></li>
+          <li className='head-nav__item'><Link className='head-nav__item__link' to='/about'>About</Link></li>
+        </ul>
+      </header>
+    )
+  }
 }
